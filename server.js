ab3103e03e545b2cd19a03111f2c102e703c2d3f
Sergey Trasko <sergey.trasko@gmail.com>
2017-02-27 21:26:10 +0200

    readme + gitignore

--- server.js
+++ server.js
@@ -4,5 +4,6 @@ require('babel-register')();
 
 const app = require('./src/app');
 const port = process.env.PORT || 3000;
+console.log(`starting app on port ${port}`);
 
 app.listen(port, () => console.log(`Koa application listening on port ${port}`));
